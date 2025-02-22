a8f9d17c4396653892ffa42b2606aba3f5d3adb9
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-22 11:53:23 +0530

    comments added

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ import ollama
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # loads a value from our environment variables
+# Load environment variables from .env file
+load_dotenv()
 
+# Dictionary to store API keys and their associated credits
 API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
 
+# Dependency function to verify API key and check available credits
 def verify_api_key(api_key: str = Header(None)):
     credits = API_KEY_CREDITS.get(api_key, 0)
     if credits <= 0:
+        # Raise an error if the API key is invalid or has no credits left
         raise HTTPException(status_code=401, detail="Invalid API Key, OR No Credits")
     
     return api_key
 
+# Initialize FastAPI application
 app = FastAPI()
 
 @app.post("/generate")
 def generate(prompt: str, api_key: str = Depends(verify_api_key)):
+     # Deduct one credit from the API key's balance
     API_KEY_CREDITS[api_key] -= 1
+
+    # Call the Ollama API to generate a response based on the provided prompt
     response = ollama.chat(model="deepseek-r1:8b", messages=[{"role": "user", "content": prompt}])
+
+    # Return the generated response
     return{"response": response["message"]["content"]}
