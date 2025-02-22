048445acadf7528bdaaa1eeec93a524d5da660a8
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-22 11:27:47 +0530

    secured the api

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ load_dotenv() # loads a value from our environment variables
 
 API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
 
+def verify_api_key(api_key: str = Header(None)):
+    credits = API_KEY_CREDITS.get(api_key, 0)
+    if credits <= 0:
+        raise HTTPException(status_code=401, detail="Invalid API Key, OR No Credits")
+    
+    return api_key
+
 app = FastAPI()
 
 @app.post("/generate")
-def generate(prompt: str):
+def generate(prompt: str, api_key: str = Depends(verify_api_key)):
+    API_KEY_CREDITS[api_key] -= 1
     response = ollama.chat(model="deepseek-r1:8b", messages=[{"role": "user", "content": prompt}])
     return{"response": response["message"]["content"]}
