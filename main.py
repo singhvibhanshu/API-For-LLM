c24cfbf07a548485c5596b10a9d755e42baeec87
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-22 11:01:15 +0530

    http post added

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from fastapi import FastAPI
 import ollama
+
+app = FastAPI()
+
+@app.post("/generate")
+def generate(prompt: str):
+    response = ollama.chat(model="deepseek-r1:8b", messages=[{"role": "user", "content": prompt}])
+    return{"response": response["message"]["content"]}
